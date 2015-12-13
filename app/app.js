5e18a9bafdfd9b389842e2cf5bdc1e1d03660e8e
=guedj_m <maxime.guedj@epitech.eu>
2015-12-13 06:39:37 +0100

    starting view

--- app/app.js
+++ app/app.js
@@ -1,6 +1,31 @@
+var config = require('../config');
 var express = require('express');
 
+var info = require('debug')('auth:info');
+var err = require('debug')('auth:error');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
 var app = express();
+
+//Setting render engine
+app.set('views', './app/view');
+app.set('view engine', 'jade');
+
+//Setting middleware
+app.use(express.static('./app/public/'));
+app.use(session({secret: config.server.session_secret, resave: true, saveUninitialized: false}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+//Routes
+var login = require('./route/login');
+var authorize = require('./route/authorize');
+
+app.use('/v1/oauth2/login', login);
+app.use('/v1/oauth2/authorize', authorize);
 
 // 404 error
 app.use(function(req, res, next) {
